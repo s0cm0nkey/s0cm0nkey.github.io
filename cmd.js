139c24004dfb43871575087d6b6e04d749ba07f4
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-21 21:22:19 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -1,6 +1,10 @@
-function displayUpperCase() 
+  function identifyIPAddress(textInput) 
   {
-    let textInput = document.getElementById("textInput").value;
-    let textOutput = textInput.toUpperCase();
-    document.getElementById("textOutput").innerHTML = textOutput;
+    const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+    const matches = textInput.match(ipRegex);
+    if (matches && matches.length > 0) {
+      return matches[0];
+    } else {
+      return 'No IP address found';
+    }
   }
