56d5064050449d32dfc719cdc1fa63d485877b11
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-21 22:05:12 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -7,15 +7,26 @@ function displayUpperCase()
 function identifyIPAddress() 
   {
     var textInput = document.getElementById("textInput").value;
-    var textOutput = "";
+    var textOutput = textInput;
     const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
     const matches = textInput.match(ipRegex);
     if (matches && matches.length > 0) {
       matches.forEach(match => {
        textOutput += textInput.replace(match, `<span style="background-color: yellow;">${match}</span>`);
+      }); 
+    } 
+    document.getElementById("textOutput").innerHTML = textOutput;
+  }
+  function ipAddressHighlight(textInput) {
+    let outputText = "";
+    const ipAddressRegex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g;
+    const matches = textInput.match(ipAddressRegex);
+    if (matches) {
+      matches.forEach(match => {
+        outputText += textInput.replace(match, `<span style="background-color: yellow;">${match}</span>`);
       });
     } else {
-      textOutput = textInput;
+      outputText = textInput;
     }
-    document.getElementById("textOutput").innerHTML = textOutput;
+    return outputText;
   }
