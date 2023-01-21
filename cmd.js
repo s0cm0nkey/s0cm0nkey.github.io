60013e514f44e9526ab8214e6fc5564700ce4f13
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-21 21:29:54 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -1,10 +1,19 @@
-  function identifyIPAddress(textInput) 
+function displayUpperCase() 
   {
+    let textInput = document.getElementById("textInput").value;
+    let textOutput = textInput.toUpperCase();
+    document.getElementById("textOutput").innerHTML = textOutput;
+  }
+  function identifyIPAddress() 
+  {
+    var textInput = document.getElementById("textInput").value;
+    var textOutput = textInput;
     const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
     const matches = textInput.match(ipRegex);
     if (matches && matches.length > 0) {
-      return matches[0];
+       textOutput = matches[0];
     } else {
-      return 'No IP address found';
+      textOutput =  'No IP address found';
     }
+    document.getElementById("textOutput").innerHTML = textOutput;
   }
