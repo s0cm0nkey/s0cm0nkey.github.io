eae55975d2fe49abd478692d726a3c53e26b3139
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-21 21:42:32 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -4,16 +4,31 @@ function displayUpperCase()
     let textOutput = textInput.toUpperCase();
     document.getElementById("textOutput").innerHTML = textOutput;
   }
-  function identifyIPAddress() 
+function identifyIPAddress() 
   {
     var textInput = document.getElementById("textInput").value;
     var textOutput = textInput;
     const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
     const matches = textInput.match(ipRegex);
     if (matches && matches.length > 0) {
-       textOutput = matches[0];
+      matches.forEach(match => {
+       textOutput += textInput.replace(match, `<span style="background-color: yellow;">${match}</span>`);
+      });
     } else {
-      textOutput =  'No IP address found';
+      textOutput =  textInput;
+    }
+    document.getElementById("textOutput").innerHTML = textOutput;
+  }
+function ipAddressHighlight(textInput) {
+    let textOutput = "";
+    const ipAddressRegex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g;
+    const matches = textInput.match(ipAddressRegex);
+    if (matches) {
+      matches.forEach(match => {
+        textOutput += textInput.replace(match, `<span style="background-color: yellow;">${match}</span>`);
+      });
+    } else {
+      textOutput = textInput;
     }
     document.getElementById("textOutput").innerHTML = textOutput;
   }
