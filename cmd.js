4ae6b30193159a1421463cfcbb1d12a6c683a496
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-22 04:55:52 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -26,8 +26,8 @@ function highlight() {
   //replace all items in the textInput with spans of highlighting
   Output = Output.replace(ipRegEx, (match) => `<mark style="background-color:yellow;">${match}</mark>`);
   Output = Output.replace(dnameRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
-  Output = Output.replace(uriRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
-  Output = Output.replace(filepath, (match) => `<mark style="background-color:coral;">${match}</mark>`);
+  Output = Output.replace(uriRegEx, (match) => `<mark style="background-color:grey;">${match}</mark>`);
+  Output = Output.replace(filepath, (match) => `<mark style="background-color:grey;">${match}</mark>`);
   Output = Output.replace(filename, (match) => `<mark style="background-color:coral;">${match}</mark>`);
   Output = Output.replace(wincmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
   Output = Output.replace(bashcmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
