93f48d9060dcc20ef99d239b2a9d6ed8e08ec83d
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-01-22 02:52:31 +0000

    dev

--- cmd.js
+++ cmd.js
@@ -25,13 +25,13 @@ function highlight() {
   var lolMatch = textInput.match(lolbas);
   //replace all items in the textInput with spans of highlighting
   Output = Output.replace(ipRegEx, (match) => `<mark style="background-color:yellow;">${match}</mark>`);
-  Output = Output.replace(dnameRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
-  Output = Output.replace(uriRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
-  Output = Output.replace(filepath, (match) => `<mark style="background-color:coral;">${match}</mark>`);
-  Output = Output.replace(filename, (match) => `<mark style="background-color:coral;">${match}</mark>`);
-  Output = Output.replace(wincmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
-  Output = Output.replace(bashcmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
-  Output = Output.replace(pscmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
+  //Output = Output.replace(dnameRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
+  //Output = Output.replace(uriRegEx, (match) => `<mark style="background-color:green;">${match}</mark>`);
+  //Output = Output.replace(filepath, (match) => `<mark style="background-color:coral;">${match}</mark>`);
+  //Output = Output.replace(filename, (match) => `<mark style="background-color:coral;">${match}</mark>`);
+  //Output = Output.replace(wincmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
+  //Output = Output.replace(bashcmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
+  //Output = Output.replace(pscmd, (match) => `<mark style="background-color:deepskyblue;">${match}</mark>`);
   //return the output text
   document.getElementById("textOutput").innerHTML = Output;
   document.getElementById("ipAddresses").innerHTML = ipMatches;
